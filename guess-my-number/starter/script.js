357cb08da8e47a1ccb8c0019a71139fb0de043d7
Shivam-Kansagara <shivamkansagara1@gmail.com>
2024-07-28 00:34:06 +0530

    refactoring code

--- guess-my-number/starter/script.js
+++ guess-my-number/starter/script.js
@@ -3,40 +3,34 @@
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let intialHighScore = 0;
-document.querySelector('.number').textContent = randomNumber;
 
 document.querySelector('.again').addEventListener('click', () => {
   window.location.reload();
 });
 
-document.querySelector('.check').addEventListener('click', () => {
+function handleGuessedNumber(message) {
+  if (score <= 1) {
+    document.querySelector('.message').textContent = 'you Lost!';
+    score = 0;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent = message;
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+  intialHighScore++;
+}
+
+function handleClick() {
   if (document.querySelector('.check').textContent == 'Check!') {
     const guess = Number(document.querySelector('.guess').value);
     if (!guess) {
       document.querySelector('.message').textContent = 'not a valid number';
     } else {
       if (guess > randomNumber) {
-        if (score <= 1) {
-          document.querySelector('.message').textContent = 'you Lost!';
-          score = 0;
-          document.querySelector('.score').textContent = score;
-        } else {
-          document.querySelector('.message').textContent = 'Too High!';
-          score--;
-          document.querySelector('.score').textContent = score;
-        }
-        intialHighScore++;
+        handleGuessedNumber('Too High!');
       } else if (guess < randomNumber) {
-        if (score <= 1) {
-          document.querySelector('.message').textContent = 'you Lost!';
-          score = 0;
-          document.querySelector('.score').textContent = score;
-        } else {
-          document.querySelector('.message').textContent = 'Too Low!';
-          score--;
-          document.querySelector('.score').textContent = score;
-        }
-        intialHighScore++;
+        handleGuessedNumber('Too Low!');
       } else {
         document.querySelector('.message').textContent = 'You Won!!';
         document.querySelector('.highscore').textContent = Math.max(
@@ -44,6 +38,8 @@ document.querySelector('.check').addEventListener('click', () => {
           document.querySelector('.highscore').textContent
         );
         document.querySelector('.check').textContent = 'Play Again';
+        document.querySelector('body').style.backgroundColor = '#6666ff';
+        document.querySelector('.number').textContent = randomNumber;
       }
     }
   } else {
@@ -53,5 +49,13 @@ document.querySelector('.check').addEventListener('click', () => {
     intialHighScore = 0;
     score = 20;
     document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').textContent = '?';
+  }
+}
+document.querySelector('.check').addEventListener('click', handleClick);
+document.querySelector('.guess').addEventListener('keydown', () => {
+  if (event.key === 'Enter') {
+    handleClick();
   }
 });
