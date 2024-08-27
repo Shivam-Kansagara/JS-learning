0475a51fffe125bbc16a05c8ead707898fda697d
Shivam-Kansagara <shivamkansagara1@gmail.com>
2024-08-28 01:24:16 +0530

    feat : intersection oberserver with img and sections

--- bankist/code-fe/script.js
+++ bankist/code-fe/script.js
@@ -32,6 +32,8 @@ document.addEventListener('keydown', function (e) {
 
 const scrollButton = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const section2 = document.querySelector('#section--2');
+const section3 = document.querySelector('#section--3');
 
 scrollButton.addEventListener('click', () => {
   section1.scrollIntoView({ behavior: 'smooth' });
@@ -74,4 +76,79 @@ operationsTabContainer.addEventListener('click', function (e) {
       .querySelector(`.operations__content--${tabNumber}`)
       .classList.add('operations__content--active');
   }
+});
+
+const navLink = document.querySelectorAll('.nav__link');
+
+const handlerOver = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    navLink.forEach(function (val) {
+      if (val !== e.target) {
+        val.style.opacity = this; // cannot use this keyword directly inside foreach if want to use external vars then pass in as third argument.
+      }
+    }, this);
+  }
+};
+document
+  .querySelector('.nav__links')
+  .addEventListener('mouseover', handlerOver.bind(0.5));
+
+document
+  .querySelector('.nav__links')
+  .addEventListener('mouseout', handlerOver.bind(1));
+
+const headerOptions = {
+  root: null,
+  threshold: 0,
+};
+function headerCallback(entries, observer) {
+  if (entries[0].isIntersecting === false) {
+    header.querySelector('.nav').classList.add('sticky');
+  } else {
+    header.querySelector('.nav').classList.remove('sticky');
+  }
+}
+const headerObserver = new IntersectionObserver(headerCallback, headerOptions);
+const header = document.querySelector('.header');
+headerObserver.observe(header);
+
+function sectionCallback(entries, observer) {
+  if (entries[0].isIntersecting) {
+    document
+      .querySelector(`#${entries[0].target.id}`)
+      .classList.remove('section--hidden');
+  } else {
+    document
+      .querySelector(`#${entries[0].target.id}`)
+      .classList.add('section--hidden');
+  }
+}
+const sectionOptions = {
+  root: null,
+  threshold: 0.1,
+};
+const sectionObserver = new IntersectionObserver(
+  sectionCallback,
+  sectionOptions
+);
+sectionObserver.observe(section1);
+sectionObserver.observe(section2);
+sectionObserver.observe(section3);
+
+function imgCallback(entries, oberserver) {
+  if (entries[0].isIntersecting) {
+    entries[0].target.src = entries[0].target.dataset.src;
+    entries[0].target.classList.remove('lazy-img');
+  } else {
+    entries[0].target.classList.add('lazy-img');
+  }
+}
+const imgOptions = {
+  root: null,
+  threshold: 0.5,
+};
+const imgObserver = new IntersectionObserver(imgCallback, imgOptions);
+const featuresImg = document.querySelectorAll('img[data-src]');
+featuresImg.forEach(val => {
+  imgObserver.observe(val);
 });
